694a580c98fa6ade0282811508bf2d565f30ef85
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-03-25 21:08:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Cards from "./components/cards";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Game-REACT</h1>
+      <Cards/>
+      
+
+    </div>
+  );
+}
+
+export default App;
